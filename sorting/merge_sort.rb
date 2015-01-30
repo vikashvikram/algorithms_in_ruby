e4ce672b4d1c5710dc50ecefbf94c65d2998245c
Vikash Vikram <vikashvikram@gmail.com>
2015-01-30 04:28:12 -0500

    merge sort changes

--- sorting/merge_sort.rb
+++ sorting/merge_sort.rb
@@ -1,4 +1,20 @@
 module MergeSort
   def merge_sort
+    partial_merge_sort(0, length-1)
+  end
+
+  private
+  def partial_merge_sort(start, ending)
+    if ending > start
+      mid = (start + ending )/2
+      partial_merge_sort(start, mid) 
+      partial_merge_sort(mid+1, ending)
+      merge(start, mid, ending)
+    else
+      self[start]
+    end 
+  end
+
+  def merge(start, mid, ending)
   end
 end
