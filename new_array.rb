547bedf81c3ce0960774a9965c65afb5ed91162f
Vikash Vikram <vikashvikram@gmail.com>
2015-02-02 04:36:58 -0500

    adding insertion sort

--- new_array.rb
+++ new_array.rb
@@ -1,4 +1,5 @@
 require_relative 'sorting/selection_sort'
+require_relative 'sorting/insertion_sort'
 require_relative 'sorting/merge_sort'
 require_relative 'searching/binary_search'
 require_relative 'searching/sequential_search'
@@ -8,6 +9,7 @@ class NewArray < Array
 
 #Include sorting algorithms
 include SelectionSort
+include InsertionSort
 include MergeSort
 
 include ArrayInversion
--- sorting/insertion_sort.rb
+++ sorting/insertion_sort.rb
@@ -1,5 +1,15 @@
 module InsertionSort
 
   def insertion_sort
+    if length > 1
+      1.upto(length-1) do |index|
+        val = self[index]
+        (index-1).downto(0) do |index2|
+          break unless self[index2] >= val 
+          self[index2+1], self[index2] = self[index2], val
+        end
+      end
+    end
+    self
   end
 end
