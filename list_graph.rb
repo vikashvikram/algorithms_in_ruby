eb58c1d441552c4d012325fd944f7224eb1f7e28
Vikash Vikram <vikashvikram@gmail.com>
2015-02-21 19:24:37 +0530

    graph implementation using adjacency list data structure

---
+++ list_graph.rb
@@ -0,0 +1,80 @@
+#ADJACENCY LIST datastructure
+#undirected graph
+class Graph
+	VERTEX_COUNT = 200
+	INPUT_FILE = 'kargerMinCut.txt' #tab separatedintergers in each line ranging from 1 to VERTEX_COUNT 
+	attr_reader :vertices, :edges
+	def initialize(number_of_vertices = VERTEX_COUNT)
+		@vertices, @edges = Array.new(number_of_vertices), Array.new(number_of_vertices)
+		number_of_vertices.times do |x|
+			@vertices[x] = Node.new(x+1)
+			@edges[x] = []
+		end
+	end
+
+	def vertex(label)
+		@vertices[label-1]
+	end
+
+	def add_vertex(node)
+		vertex[node.label] = node
+	end
+
+	def remove_vertex(node)
+		vertex(node.label).edges.each do |e|
+			remove_edge(e)
+		end
+		vertex[node.label] = nil
+	end
+
+	def add_edge(node1,node2)
+		e = Edge.new(node1.label,node2.label)
+		node1.add_edge(e)
+		node2.add_edge(e)
+	end
+
+	def remove_edge(edge)
+		node_labels = edge.node_labels
+		node_labels.each do |label|
+			vertex(label).remove_edge(edge)
+		end
+	end
+
+	class Node
+		attr_reader :label
+		def initialize(label)
+			@label = label
+		end
+
+		def edges
+			@edges[label-1]
+		end
+
+		def add_edge(e)
+			(edges << e).uniq!
+		end
+
+		def remove_edge(e)
+			edges.delete(e)
+		end
+		
+		def ==(node)
+			label == node.label rescue false
+		end
+	end
+
+	class Edge
+		attr_reader :node_labels
+		def initialize(u,v)
+			@node_labels = [u,v]
+		end
+
+		def vertices
+			node_labels.sort
+		end
+
+		def ==(edge)
+			vertices == edge.vertices rescue false
+		end
+	end
+end
