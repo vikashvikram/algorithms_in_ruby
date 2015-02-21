3c50014b5820a5b285ea2757d2efa5a2b814c921
Vikash Vikram <vikashvikram@gmail.com>
2015-02-22 01:27:16 +0530

    adding directed graph and fixing bugs

--- list_graph.rb
+++ list_graph.rb
@@ -1,6 +1,6 @@
 #ADJACENCY LIST datastructure
 #undirected graph
-class Graph
+class UndirectedGraph
 	VERTEX_COUNT = 200
 	INPUT_FILE = 'kargerMinCut.txt' #tab separatedintergers in each line ranging from 1 to VERTEX_COUNT 
 	attr_reader :vertices, :edges
@@ -17,14 +17,16 @@ class Graph
 	end
 
 	def add_vertex(node)
-		vertex[node.label] = node
+		label = vertices.count+1
+		@edges[label-1] = []
+		@vertices[label-1] = Node.new(label, @edges[label-1])
 	end
 
 	def remove_vertex(node)
 		vertex(node.label).edges.each do |e|
 			remove_edge(e)
 		end
-		vertex[node.label] = nil
+		@vertices[node.label-1] = nil
 	end
 
 	def add_edge(node1,node2)
@@ -42,7 +44,7 @@ class Graph
 
 	class Node
 		attr_reader :label, :edges
-		def initialize(label, edges)
+		def initialize(label=nil, edges=nil)
 			@label = label
 			@edges = edges
 		end
@@ -72,6 +74,100 @@ class Graph
 
 		def ==(edge)
 			vertices == edge.vertices rescue false
+		end
+	end
+end
+
+#ADJACENCY LIST datastructure
+#directed graph
+class DirectedGraph
+	VERTEX_COUNT = 200
+	INPUT_FILE = 'kargerMinCut.txt' #tab separatedintergers in each line ranging from 1 to VERTEX_COUNT 
+	attr_reader :vertices, :edges, :incoming_edges
+	def initialize(number_of_vertices = VERTEX_COUNT)
+		@vertices, @edges, @incoming_edges = Array.new(number_of_vertices), Array.new(number_of_vertices), Array.new(number_of_vertices)
+		number_of_vertices.times do |x|
+			@edges[x] = []
+			@incoming_edges[x] = []
+			@vertices[x] = Node.new(x+1, @edges[x], @incoming_edges[x])
+		end
+	end
+
+	def vertex(label)
+		@vertices[label-1]
+	end
+
+	def add_vertex(node)
+		label = vertices.count+1
+		@edges[label-1] = []
+		@incoming_edges[label-1] = []
+		@vertices[label-1] = Node.new(label, @edges[label-1], @incoming_edges[label-1])
+	end
+
+	def remove_vertex(node)
+		vertex(node.label).edges.each do |e|
+			remove_edge(e)
+		end
+		vertex(node.label).incoming_edges.each do |e|
+			remove_edge(e)
+		end
+		@vertices[node.label-1] = nil
+	end
+
+	def add_edge(node1,node2)
+		e = Edge.new(node1.label,node2.label)
+		if vertex(node1.label) and vertex(node2.label)
+			node1.add_edge(e)
+			node2.add_incoming_edge(e)
+		end
+	end
+
+	def remove_edge(edge)
+		vertex(edge.tail).remove_edge(edge)
+		vertex(edge.head).remove_incoming_edge(edge)
+	end
+
+	class Node
+		attr_reader :label, :edges, :incoming_edges
+		def initialize(label=nil, edges=nil, incoming_edges=nil)
+			@label = label
+			@edges = edges
+			@incoming_edges = incoming_edges
+		end
+
+		def add_edge(e)
+			(edges << e).uniq!
+		end
+
+		def remove_edge(e)
+			edges.delete(e)
+		end
+
+		def add_incoming_edge(e)
+			(incoming_edges << e).uniq!
+		end
+
+		def remove_incoming_edge(e)
+			incoming_edges.delete(e)
+		end
+		
+		def ==(node)
+			label == node.label rescue false
+		end
+	end
+
+	class Edge
+		attr_reader :node_labels, :head, :tail
+		def initialize(u,v)
+			@node_labels, @head, @tail = [u,v], v, u
+		end
+
+		def vertices
+			node_labels.sort
+		end
+
+		def ==(edge)
+			(head == edge.head and tail == egde.tail) rescue false
 		end
 	end
 end
