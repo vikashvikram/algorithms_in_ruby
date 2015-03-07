04b0fce2c48173cc0444b36b0e46d2a11d7750a1
Vikash Vikram <vikashvikram@gmail.com>
2015-03-07 12:18:44 -0500

    restructuring code pieces

--- ds/heap.rb
+++ ds/heap.rb
@@ -1,4 +1,4 @@
-class Heap
+class MinHeap
 	attr_reader :heap
 	def initialize(arr)
 		@heap = arr.unshift(nil)
@@ -16,9 +16,17 @@ class Heap
 		return min
 	end
 
+	def min
+		heap[1]
+	end
+
+	def size
+		@heap.length-1
+	end
+
 	def insert(val)
 		heap[heap.length], parent, current = val, parent(heap.length), heap.length
-		unless heap[parent] <= val
+		until heap[parent] <= val
 			swap(current, parent)
 			current = parent
 			parent = parent(parent)
@@ -56,15 +64,69 @@ class Heap
 	end
 
 end
+class MaxHeap
+	attr_reader :heap
+	def initialize(arr)
+		@heap = arr.unshift(nil)
+		(@heap.length/2).downto(1) do |i|
+			heapify(i)
+		end
+	end
 
-h = Heap.new([5,8,3,2,6,1,0,7,9,4])
-puts "#{h.extract_min}----#{h.heap}"
-puts "#{h.extract_min}----#{h.heap}"
-puts "#{h.extract_min}----#{h.heap}"
-puts "#{h.extract_min}----#{h.heap}"
-puts "#{h.extract_min}----#{h.heap}"
-puts "#{h.extract_min}----#{h.heap}"
-puts "#{h.insert(10)}----#{h.heap}"
-puts "#{h.insert(14)}----#{h.heap}"
-puts "#{h.insert(12)}----#{h.heap}"
-puts "#{h.extract_min}----#{h.heap}"
+	def extract_max
+		max, length = heap[1], heap.length
+		heap[1] = nil
+		swap(1, length-1)
+		heapify(1)
+		heap.pop
+		return max
+	end
+
+	def max
+		heap[1]
+	end
+
+	def size
+		@heap.length-1
+	end
+
+	def insert(val)
+		heap[heap.length], parent, current = val, parent(heap.length), heap.length
+		until heap[parent] >= val
+			swap(current, parent)
+			current = parent
+			parent = parent(parent)
+		end
+	end
+
+
+	private
+	def parent(i)
+		i == 1 ? 1 : i/2
+	end
+
+	def left(i)
+		2*i
+	end
+
+	def right(i)
+		2*i+1
+	end
+
+	def heapify(i)
+		left, right,maximum = left(i), right(i), i
+		maximum = left if heap[left] and heap[left] > heap[maximum]
+		maximum = right if heap[right] and heap[right] > heap[maximum]
+		unless maximum == i
+			swap(maximum, i)
+			heapify(maximum)
+		end
+	end
+
+	def swap(i,j)
+		temp  = heap[j]
+		heap[j] = heap[i]
+		heap[i] = temp
+	end
+
+end
